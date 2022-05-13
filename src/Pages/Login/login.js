1032c7bbcee08b0b6210a18e385cd636384cfeab
Ilhambint <ilhambintangp7@gmail.com>
2022-05-13 20:59:56 +0700

    second

--- src/Pages/Login/login.js
+++ src/Pages/Login/login.js
@@ -8,26 +8,26 @@ const login = () => {
                 <div className='image-login'>
                     <img className='login-img' src='images/Login-car.jpg'/>
                 </div>
-           <div className='col'>
-            <form>
-                <label>
-                    <p>Username</p>
-                    <input type="text"/>
-                </label>
+                <div className='col'>
+                    <form>
+                        <label>
+                            <p>Username</p>
+                            <input type="text"/>
+                        </label>
 
-                <label>
-                    <p>Password</p>
-                    <input type="password"/>
-                </label>
-                <div>
-                    <button type="submit">Submit</button>
+                        <label>
+                            <p>Password</p>
+                            <input type="password"/>
+                        </label>
+                        <div>
+                            <button type="submit">Submit</button>
+                        </div>
+                    </form>
                 </div>
-            </form>
-            </div>
 
             </div>
         </div>
-    );
+    ) :
 }
 
 export default login
